90b3a3d15f46815732c0778d521aeaa1dd3a4033
Egorturtsakov <97161946+Egorturtsakov@users.noreply.github.com>
2022-07-07 21:54:19 +0300

    Add files via upload

---
+++ Homework_5.js
@@ -0,0 +1,32 @@
+function addition(a, b) {
+    return a + b;
+}
+function subtraction(a, b) {
+    return a - b;
+}
+function division(a, b) {
+    return a / b;
+}
+function multiplication(a, b) {
+    return a * b;
+}
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case "+":
+            return addition(arg1, arg2);
+        case "-":
+            return subtraction(arg1, arg2);
+        case "/":
+            return division(arg1, arg2);
+        case "*":
+            return multiplication(arg1, arg2);
+        default:
+            throw new Error("Операция " + operation + " не предусмотрена");
+    }
+}
+
+console.log(mathOperation(3, 5, "+"));
+console.log(mathOperation(3, 5, "-"));
+console.log(mathOperation(3, 5, "/"));
+console.log(mathOperation(3, 5, "*"));
+console.log(mathOperation(3, 5, "lorem"));
---
+++ Homework_6.js
@@ -0,0 +1,34 @@
+function getCase(num) {
+    let beforeLastDigit = getDigitBeforeLastDigit(num);
+    if (beforeLastDigit == 1) {
+        return "рублей";
+    }
+    num = String(num);
+    let lastNumber = Number(num.charAt(num.length - 1));
+    switch (lastNumber) {
+        case 0:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            return "рублей";
+        case 1:
+            return "рубль";
+        case 2:
+        case 3:
+        case 4:
+            return "рубля";
+    }
+}
+function getDigitBeforeLastDigit(num) {
+    num = String(num);
+    let digit = num.charAt(num.length - 2);
+    if (digit !== "") {
+        return Number(digit);
+    }
+    return null;
+}
+
+let money = parseInt(prompt("Сколько деньжат вы хотите положить на свой счет?"));
+alert(`Ваша вся сумма ${money} ${getCase(money)} успешно пополненна.`);
